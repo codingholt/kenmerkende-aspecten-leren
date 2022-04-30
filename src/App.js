bb61010da527faffc620c863f105f56c89a3632a
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-04-30 16:09:43 +0200

    removed form so you can answer the question again if false

--- src/App.js
+++ src/App.js
@@ -5,32 +5,63 @@ import { useState, useEffect } from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Question from './components/Question';
-import UserInput from './components/UserInput';
 
 
-const makeQuestion = async () =>{
+
+const getQuestion = async () =>{
   const randomNumber = Math.floor(Math.random() * (32 - 1) + 1)
-  const question = await axios.get(`http://localhost:3000/questions/${randomNumber}`)
+  const question = await axios.get(`https://ka-backend.herokuapp.com/questions/${randomNumber}`)
   return question.data
 }
 
+const FalseAnswer = () =>
+  <div className="terminal-alert terminal-alert-error">Sorry, je antwoord is fout</div>;
+
+const CorrectAnswer = () =>
+  <div className="terminal-alert terminal-alert-error">Goed Antwoord!</div>;
+
+
 function App() {
   const [question, setQuestion] = useState('')
+  const [input, setInput] = useState('')
+  const [showErr, setShowErr] = useState()
+
+  const checkAnswer = (input) => {
+    if(input === question.answer){
+      setShowErr(false)
+      getQuestion().then((response) => setQuestion(response))
+    }
+    setShowErr(true)
+
+  }
 
 
   useEffect(() => {
 
-    makeQuestion().then((response) => setQuestion(response))
+    getQuestion().then((response) => setQuestion(response))
 
   }, [])
-  console.log(question)
 
+  const handleInput = (e) =>{
+    e.preventDefault();
+    setInput(e.target.value)
+  }
   return (
   <div className='container'>
     <Header/>
     {/* <h1>Leer de kenmermerkende aspecten.</h1> */}
     <Question question={question.question}/>
-    <UserInput/>
+    <div className='input_wrapper'>
+          
+        <input value={input} type="text" placeholder='kenmerkend aspect' className='text_input block' onChange={ handleInput }>
+
+        </input>
+
+        <button type="submit" className='btn btn-primary btn-block' onClick={() => {checkAnswer(input)}}>Submit answer</button>
+    
+       
+    </div>
+     {showErr ? <FalseAnswer/> : console.log('👍')}
     <Footer/>
   </div>
   );
