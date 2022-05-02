4d19eb7c63393e318406f8532395310af4fed28e
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-05-02 10:39:44 +0200

    implemented search on false answer

--- src/App.js
+++ src/App.js
@@ -7,13 +7,33 @@ import { useState, useEffect } from 'react';
 // import Footer from './components/Footer';
 import Header from './components/Header';
 import Question from './components/Question';
+import Fuse from 'fuse.js';
 
 
 
 const getQuestion = async () =>{
   const randomNumber = Math.floor(Math.random() * (32 - 1) + 1)
   const question = await axios.get(`https://ka-backend.herokuapp.com/questions/${randomNumber}`)
+
   return question.data
+
+}
+
+const getSuggestion = async (input) => {
+  const list = await axios.get('https://ka-backend.herokuapp.com/questions/');
+  const data = list.data
+
+  const options = {
+    includeScore: true,
+    // 
+    keys: ['answer']
+  }
+
+  const fuse = new Fuse(data, options)
+  console.log(input)
+  const search = await fuse.search(input)
+  console.log(search)
+
 }
 
 
@@ -23,7 +43,7 @@ function App() {
   const [input, setInput] = useState('')
 
 
-  const checkAnswer = (input) => {
+  const checkAnswer = async (input) => {
     if(input.toLowerCase() === question.answer.toLowerCase()){
       toast.success('Goed geantwoord 🎉', {
         position: "top-right",
@@ -46,6 +66,8 @@ function App() {
       draggable: true,
       progress: undefined,
   })
+  await getSuggestion(input)
+
   setInput('')
 }
 }
